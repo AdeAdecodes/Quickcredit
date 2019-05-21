665e926c8f1a93921a66a62683c9d7a47ad744fd
hardecx <31935467+hardecx@users.noreply.github.com>
2019-05-21 11:17:46 +0100

    Chore setup database
    Installed pg package
    setup database configuration
    Created the required table structure
    Added default admin
    
    [Delivers #166104894]

---
+++ server/migration/createTable.js
@@ -0,0 +1,22 @@
+import query from './index';
+
+
+const queryString = `
+
+  CREATE TABLE IF NOT EXISTS users(
+
+    id BIGSERIAL PRIMARY KEY,      
+    firstName VARCHAR(128) NOT NULL,
+    lastName VARCHAR(128) NOT NULL,
+    homeAddress VARCHAR(128) NOT NULL,
+    workAddress VARCHAR(128) NOT NULL,
+    phoneNumber VARCHAR(128) NOT NULL,
+    email VARCHAR(128) UNIQUE NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    registered TIMESTAMP,
+    status VARCHAR(128) NOT NULL,
+    isAdmin BOOLEAN NOT NULL
+
+  );`;
+
+query(queryString);
